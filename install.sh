3769180e657ec553cd3a52d7386e431385dc1daf
Anatoly S <50491171+Tolyak26@users.noreply.github.com>
2022-02-12 19:26:25 +0700

    Add files via upload

--- install.sh
+++ install.sh
@@ -6,25 +6,26 @@ SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 # Import settings from setup.conf
 source $SCRIPT_DIR/setup.conf
 
+chmod +x 01-pre-setup.sh
+chmod +x 02-setup.sh
+chmod +x 03-user.sh
+chmod +x 04-post-setup.sh
+
 echo "Launch 01-pre-setup.sh"
 echo ""
-chmod +x 01-pre-setup.sh
 bash 01-pre-setup.sh
 echo ""
 
 echo "Launch 02-setup.sh in root folder"
 echo ""
-chmod +x 02-setup.sh
 arch-chroot /mnt /root/arch-linux-install/02-setup.sh
 echo ""
 
 echo "Launch 03-user.sh in $USERNAME's home folder"
 echo ""
-chmod +x 03-user.sh
 arch-chroot /mnt /usr/bin/runuser -u $USERNAME -- /home/$USERNAME/arch-linux-install/03-user.sh
 echo ""
 
 echo "Launch 04-post-setup.sh in root folder"
 echo ""
-chmod +x 04-post-setup.sh
 arch-chroot /mnt /root/arch-linux-install/04-post-setup.sh
