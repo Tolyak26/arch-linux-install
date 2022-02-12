3cf3e7ab8b8eed7dd30501af4f8f834d8872800a
Anatoly S <50491171+Tolyak26@users.noreply.github.com>
2022-02-12 14:51:38 +0700

    Update 01-pre-setup.sh

--- 01-pre-setup.sh
+++ 01-pre-setup.sh
@@ -14,7 +14,7 @@ echo "- Setting up Yandex Arch Linux repo mirror for optimal download"
 echo ""
 pacman -S --noconfirm pacman-contrib
 sed -i 's/^#ParallelDownloads/ParallelDownloads/' /etc/pacman.conf
-echo -ne "Server = https://mirror.yandex.ru/archlinux/$repo/os/$arch" > /etc/pacman.d/mirrorlist
+echo -ne "Server = https://mirror.yandex.ru/archlinux/'$repo'/os/'$arch'" > /etc/pacman.d/mirrorlist
 chmod 644 /etc/pacman.d/mirrorlist
 
 echo "- Install Arch Linux base packages"
