257ba63ebfef3056616909bfed1f33579ab6cecb
Anatoly S <50491171+Tolyak26@users.noreply.github.com>
2022-02-12 19:21:39 +0700

    Add files via upload

--- install.sh
+++ install.sh
@@ -8,19 +8,23 @@ source $SCRIPT_DIR/setup.conf
 
 echo "Launch 01-pre-setup.sh"
 echo ""
+chmod +x 01-pre-setup.sh
 bash 01-pre-setup.sh
 echo ""
 
 echo "Launch 02-setup.sh in root folder"
 echo ""
+chmod +x 02-setup.sh
 arch-chroot /mnt /root/arch-linux-install/02-setup.sh
 echo ""
 
 echo "Launch 03-user.sh in $USERNAME's home folder"
 echo ""
+chmod +x 03-user.sh
 arch-chroot /mnt /usr/bin/runuser -u $USERNAME -- /home/$USERNAME/arch-linux-install/03-user.sh
 echo ""
 
 echo "Launch 04-post-setup.sh in root folder"
 echo ""
+chmod +x 04-post-setup.sh
 arch-chroot /mnt /root/arch-linux-install/04-post-setup.sh
