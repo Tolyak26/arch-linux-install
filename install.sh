fa934bce43bc2b2f6403b2d66ee19c8d416773e6
Anatoly S <50491171+Tolyak26@users.noreply.github.com>
2022-02-12 13:23:40 +0700

    Add files via upload

--- install.sh
+++ install.sh
@@ -4,16 +4,21 @@
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 echo -ne "Импорт настроек из конфигурационного файла setup.conf"
+echo -ne ""
 source $SCRIPT_DIR/setup.conf
 
 echo -ne "Запуск предварительного скрипта установки 01-pre-setup.sh"
+echo -ne ""
 bash 01-pre-setup.sh
 
 echo -ne "Запуск скрипта 02-setup.sh в root каталоге /"
+echo -ne ""
 arch-chroot /mnt /root/arch-linux-install/02-setup.sh
 
 echo -ne "Запуск скрипта 03-user.sh в каталоге пользователя $USERNAME"
+echo -ne ""
 arch-chroot /mnt /usr/bin/runuser -u $USERNAME -- /home/$USERNAME/arch-linux-install/03-user.sh
 
 echo -ne "Запуск скрипта 04-post-setup.sh в root каталоге /"
+echo -ne ""
 arch-chroot /mnt /root/arch-linux-install/04-post-setup.sh
