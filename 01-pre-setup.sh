b16835d118402e210d75417f5a666338c9290c28
Tolyak26 <tolyasidk26@gmail.com>
2023-05-25 07:45:00 +0700

    fixes

--- 01-pre-setup.sh
+++ 01-pre-setup.sh
@@ -66,7 +66,7 @@ echo "- Setting up Arch Linux repo mirror for optimal download ... "
 echo ""
 
 pacman -S --noconfirm --needed python3 pacman-contrib reflector
-#sed -i 's/^#ParallelDownloads/ParallelDownloads/' /etc/pacman.conf
+sed -i 's/^#ParallelDownloads/ParallelDownloads/' /etc/pacman.conf
 reflector --country Russia --age 12 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
 
 sleep 5
