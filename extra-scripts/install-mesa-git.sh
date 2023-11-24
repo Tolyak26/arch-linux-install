cddae5e630d10a155b7b9b0d43808498cfc059bb
Tolyak26 <tolyasidk26@gmail.com>
2023-11-24 14:02:14 +0700

    fixes for install-mesa-git script

--- extra-scripts/install-mesa-git.sh
+++ extra-scripts/install-mesa-git.sh
@@ -9,5 +9,7 @@ echo ""
 echo "- Installing mesa-git packages from AUR ... "
 echo ""
 
-yay -S --noconfirm --noeditmenu --needed --removemake --mflags "--skippgpcheck" mesa-git
-yay -S --noconfirm --noeditmenu --needed --removemake --mflags "--skippgpcheck" lib32-mesa-git
+yay -R --noconfirm libva-mesa-driver mesa mesa-libgl mesa-vdpau opencl-clover-mesa opencl-rusticl-mesa vulkan-intel vulkan-mesa-layers vulkan-radeon vulkan-swrast
+yay -R --noconfirm lib32-libva-mesa-driver lib32-mesa lib32-mesa-libgl lib32-mesa-vdpau lib32-vulkan-intel lib32-vulkan-mesa-layers lib32-vulkan-radeon
+
+yay -S --noconfirm --noeditmenu --needed --removemake --mflags "--skippgpcheck" mesa-git lib32-mesa-git
