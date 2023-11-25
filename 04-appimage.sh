e4142f76fc2a0d953914809315b811bb17676f6e
Tolyak26 <tolyasidk26@gmail.com>
2023-11-26 00:16:39 +0700

    fix for create AppImage desktop files

--- 04-appimage.sh
+++ 04-appimage.sh
@@ -24,17 +24,17 @@ mkdir -p /appimages
 # yuzu early access
 curl -fSs https://api.github.com/repos/pineappleEA/pineapple-src/releases/latest | grep "browser_download_url" | grep -E "Linux-Yuzu-EA-(.*).AppImage" | head -1 | cut -d '"' -f 4 | wget -O /appimages/yuzu-ea.AppImage -i -
 chmod +x /appimages/yuzu-ea.AppImage
-wget -O /usr/share/applications/yuzu-ea.desktop https://raw.githubusercontent.com/Tolyak26/arch-linux-install/master/desktop-files/yuzu-ea.desktop
+cp -v $scriptdir/desktop-files/yuzu-ea.desktop /usr/share/applications/yuzu-ea.desktop
 
 # yuzu mainline
 curl -fSs https://api.github.com/repos/yuzu-emu/yuzu-mainline/releases/latest | grep "browser_download_url" | grep -E "yuzu-mainline-(.*)-(.*).AppImage" | head -1 | cut -d '"' -f 4 | wget -O /appimages/yuzu-mainline.AppImage -i -
 chmod +x /appimages/yuzu-mainline.AppImage
-wget -O /usr/share/applications/yuzu-mainline.desktop https://raw.githubusercontent.com/Tolyak26/arch-linux-install/master/desktop-files/yuzu-mainline.desktop
+cp -v $scriptdir/desktop-files/yuzu-mainline.desktop /usr/share/applications/yuzu-mainline.desktop
 
 # MoonDeckBuddy
 curl -fSs https://api.github.com/repos/FrogTheFrog/moondeck-buddy/releases/latest | grep "browser_download_url" | grep -E "MoonDeckBuddy-(.*)-x86_64.AppImage" | head -1 | cut -d '"' -f 4 | wget -O /appimages/moondeckbuddy.AppImage -i -
 chmod +x /appimages/moondeckbuddy.AppImage
-wget -O /usr/share/applications/moondeckbuddy.desktop https://raw.githubusercontent.com/Tolyak26/arch-linux-install/master/desktop-files/moondeckbuddy.desktop
+cp -v $scriptdir/desktop-files/moondeckbuddy.desktop /usr/share/applications/moondeckbuddy.desktop
 
 chown -R $username:users /appimages
 
