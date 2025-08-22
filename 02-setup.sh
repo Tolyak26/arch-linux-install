0977723c62f709d2e856413a042d059e629a61fd
Anatoly S. <50491171+Tolyak26@users.noreply.github.com>
2025-08-22 14:56:18 +0700

    Update 02-setup.sh

--- 02-setup.sh
+++ 02-setup.sh
@@ -26,7 +26,6 @@ sed -i 's/^#ParallelDownloads/ParallelDownloads/' /etc/pacman.conf
 sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf
 #reflector --country Russia --age 12 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
 echo 'Server = https://mirror.yandex.ru/archlinux/$repo/os/$arch' > /etc/pacman.d/mirrorlist
-echo 'Server = https://mirror.yandex.ru/archlinux/$repo/os/$arch' > /etc/pacman.d/mirrorlist
 echo 'Server = https://mirror.truenetwork.ru/archlinux/$repo/os/$arch' >> /etc/pacman.d/mirrorlist
 pacman -Sy --noconfirm
 
