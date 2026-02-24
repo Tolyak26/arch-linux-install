665876ab1f897bbc32c8ae49f110d140e62bd32a
Anatoly S. <50491171+Tolyak26@users.noreply.github.com>
2026-02-24 11:29:29 +0700

    Update install-soft-pc-streaming.sh

--- extra-scripts/install-soft-pc-streaming.sh
+++ extra-scripts/install-soft-pc-streaming.sh
@@ -29,6 +29,9 @@ sudo pacman -S --noconfirm --disable-download-timeout --needed lizardbyte/sunshi
 
 sudo cp -v /usr/share/applications/dev.lizardbyte.app.Sunshine.desktop /home/${username}/.config/autostart
 sudo sed -i 's/^Terminal=true/Terminal=false/' /home/${username}/.config/autostart/dev.lizardbyte.app.Sunshine.desktop
+sudo curl -sSL https://raw.githubusercontent.com/Tolyak26/arch-linux-install/refs/heads/master/cfg-files/desktopenvironment-kde/etc/skel/config/sunshine/apps.json -o /home/${username}/.config/sunshine/apps.json
+
+sudo usermod -aG input ${username}
 
 systemctl --user enable ydotool.service
 systemctl --user start ydotool.service
@@ -40,11 +43,11 @@ sudo mkdir -p /opt/MoonDeckBuddy-appimage/
 curl -fSs https://api.github.com/repos/FrogTheFrog/moondeck-buddy/releases/latest | grep "browser_download_url" | grep -E "MoonDeckBuddy-(.*)-x86_64.AppImage" | head -1 | cut -d '"' -f 4 | sudo wget -O /opt/MoonDeckBuddy-appimage/MoonDeckBuddy.AppImage -i -
 sudo chmod +x /opt/MoonDeckBuddy-appimage/MoonDeckBuddy.AppImage
 
-sudo wget -O /usr/share/icons/hicolor/16x16/apps/MoonDeckBuddy.png https://raw.githubusercontent.com/FrogTheFrog/moondeck-buddy/main/resources/icons/app-16.png
-sudo wget -O /usr/share/icons/hicolor/32x32/apps/MoonDeckBuddy.png https://raw.githubusercontent.com/FrogTheFrog/moondeck-buddy/main/resources/icons/app-32.png
-sudo wget -O /usr/share/icons/hicolor/64x64/apps/MoonDeckBuddy.png https://raw.githubusercontent.com/FrogTheFrog/moondeck-buddy/main/resources/icons/app-64.png
-sudo wget -O /usr/share/icons/hicolor/128x128/apps/MoonDeckBuddy.png https://raw.githubusercontent.com/FrogTheFrog/moondeck-buddy/main/resources/icons/app-128.png
-sudo wget -O /usr/share/icons/hicolor/256x256/apps/MoonDeckBuddy.png https://raw.githubusercontent.com/FrogTheFrog/moondeck-buddy/main/resources/icons/app-256.png
+sudo curl -sSL https://raw.githubusercontent.com/FrogTheFrog/moondeck-buddy/main/resources/icons/app-16.png -o /usr/share/icons/hicolor/16x16/apps/MoonDeckBuddy.png
+sudo curl -sSL https://raw.githubusercontent.com/FrogTheFrog/moondeck-buddy/main/resources/icons/app-32.png -o /usr/share/icons/hicolor/32x32/apps/MoonDeckBuddy.png
+sudo curl -sSL https://raw.githubusercontent.com/FrogTheFrog/moondeck-buddy/main/resources/icons/app-64.png -o /usr/share/icons/hicolor/64x64/apps/MoonDeckBuddy.png
+sudo curl -sSL https://raw.githubusercontent.com/FrogTheFrog/moondeck-buddy/main/resources/icons/app-128.png -o /usr/share/icons/hicolor/128x128/apps/MoonDeckBuddy.png
+sudo curl -sSL https://raw.githubusercontent.com/FrogTheFrog/moondeck-buddy/main/resources/icons/app-256.png -o /usr/share/icons/hicolor/256x256/apps/MoonDeckBuddy.png
 
-sudo wget -O /usr/share/applications/MoonDeckBuddy.desktop https://raw.githubusercontent.com/Tolyak26/arch-linux-install/refs/heads/master/desktop-files/MoonDeckBuddy.desktop
+sudo curl -sSL https://raw.githubusercontent.com/Tolyak26/arch-linux-install/refs/heads/master/desktop-files/MoonDeckBuddy.desktop -o /usr/share/applications/MoonDeckBuddy.desktop
 ##
