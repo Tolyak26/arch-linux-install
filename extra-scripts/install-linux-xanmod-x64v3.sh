165503e96b87d21552ede8ccb473f4556342cbfc
Anatoly S. <50491171+Tolyak26@users.noreply.github.com>
2026-09-01 19:57:19 +0700

    Update install-linux-xanmod-x64v3.sh

--- extra-scripts/install-linux-xanmod-x64v3.sh
+++ extra-scripts/install-linux-xanmod-x64v3.sh
@@ -9,7 +9,7 @@ echo ""
 echo "- Installing linux-xanmod-x64v3 binary packages for Zen3 from AUR ... "
 echo ""
 
-sudo pacman -S --noconfirm --disable-download-timeout --needed clang llvm
+sudo pacman -S --noconfirm --disable-download-timeout --needed dkms libelf clang lld llvm
 
 #yay -S --answeredit N --answerclean N --answerdiff N --answerupgrade Y --noconfirm --needed --removemake --mflags "--skippgpcheck" linux-xanmod-linux-bin-x64v3 linux-xanmod-linux-headers-bin-x64v3
 
